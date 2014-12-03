c515e8378def55c1b9c7010d17c502aa851fcb16
Melanie L. Fryman <melanie@frymanet.com>
2014-12-02 23:21:55 -0600

    added menu button on gameover screen

--- js/gameover.js
+++ js/gameover.js
@@ -2,7 +2,7 @@
   game.state.add('gameover', {create:create, update:update});
 
   function create(){
-    // game.stage.backgroundColor = '#003060';
+    game.stage.backgroundColor = '#003060';
     exdalek = game.add.sprite(400, 250, 'exdalek');
     exdalek.animations.add('exterminate', [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23, 24], 5, false);
     game.physics.arcade.enable(exdalek);
@@ -14,7 +14,7 @@
     var title = game.add.text(400, 100, 'exterminated!', {font: "100px doctor_whoregular", fill: "#FCFCFC", align: "center" });
     title.anchor.setTo(0.5);
 
-    var button = game.add.button(400, 410, 'play', menuScreen, this);
+    var button = game.add.button(400, 410, 'menu', menuScreen, this);
     button.scale.setTo(0.5);
     button.anchor.setTo(0.5);
 
